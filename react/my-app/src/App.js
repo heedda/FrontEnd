ec31ee9aede4b78828ac49c02e887a817ddea94e
heedda <heedda.kollim@gmail.com>
2023-12-03 23:25:17 +0900

    create index 하는 중

--- react/my-app/src/App.js
+++ react/my-app/src/App.js
@@ -1,8 +1,36 @@
-import React from "react";
-import InputSample from "./inputSample";
+import React, { useRef } from "react";
+import UserLIst from "./UserLIst";
+import CreateUser from "./CreateUser";
 
 function App() {
-  return <InputSample />;
+  const users = [
+    {
+      id: 1,
+      username: "velopert",
+      email: "public.velopert@gmail.com",
+    },
+    {
+      id: 2,
+      username: "김희찬",
+      email: "heedda.kollim@gmail.com",
+    },
+    {
+      id: 3,
+      username: "조영준",
+      email: "장애",
+    },
+  ];
+
+  const nextId = useRef(4);
+  const onCreate = () => {
+    nextId.current += 1;
+  };
+  return (
+    <>
+      <CreateUser />
+      <UserLIst user={users} />
+    </>
+  );
 }
 
 export default App;
