e20a918f895561df3fac1f437ab712a2a3acd516
heedda <heedda.kollim@gmail.com>
2023-12-06 23:40:31 +0900

    useEffect

--- react/my-app/src/UserLIst.js
+++ react/my-app/src/UserLIst.js
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 function User({ user, onRemove, onToggle }) {
+  useEffect(() => {
+    console.log("user 값이 설정됨");
+    console.log(user);
+    return () => {
+      console.log("user 가 바뀌기 전..");
+      console.log(user);
+    };
+  }, [user]);
   return (
     <div>
       <b
@@ -11,7 +19,8 @@ function User({ user, onRemove, onToggle }) {
         onClick={() => onToggle(user.id)}
       >
         {user.username}
-      </b>{" "}
+      </b>
+      &nbsp;
       <span>({user.email})</span>
       <button onClick={() => onRemove(user.id)}>삭제</button>
     </div>
